d5a0f4a724c6add753b479e30874a442d09c28d5
Michail <73073160+KotelX@users.noreply.github.com>
2022-11-14 16:58:02 +0300

    Add script (Patients)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function myFunction() {;
+    var input = $('#number')[0].value;
+    var tr = $("#tabble_main tr");
+    for (i = 1; i < tr.length; i++)
+    {
+        td = tr[i].getElementsByTagName("th")[0];
+        if (td)
+        {
+            if (td.textContent.localeCompare(input) && input)
+            {
+                tr[i].style.display = "none";
+            }
+            else
+            {
+                tr[i].style.display = "";
+            }
+        }
+    }
+}
