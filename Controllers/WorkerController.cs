b8016ab522b581040372cec322d49ecad1b7c5b7
Michail <73073160+KotelX@users.noreply.github.com>
2022-11-19 18:26:54 +0300

    Add Controller

---
+++ Controllers/WorkerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wet.Controllers
+{
+    public class WorkerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
