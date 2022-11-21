ae23ae71bb4d6fde4f2d2299ae51857b47d21fda
Michail <73073160+KotelX@users.noreply.github.com>
2022-11-21 17:32:57 +0300

    Patient info edit

--- Controllers/WorkerController.cs
+++ Controllers/WorkerController.cs
@@ -29,12 +29,6 @@ namespace Wet.Controllers
         {
             return View();
         }
-        [HttpPost]
-        [Route("/login")]
-        public IActionResult Login(string login, string password)
-        {
-            return View();
-        }
 
         [Route("/PatientsInfo")]
         public IActionResult PatientsInfo(int number = 0)
@@ -61,33 +55,6 @@ namespace Wet.Controllers
         public ActionResult lk()
         {
             return View();
-        }
-        [HttpPost]
-        [Route("/login")]
-        public IActionResult Login(string login, string password)
-        {
-            return View();
-        }
-
-        [Route("/PatientsInfo")]
-        public IActionResult PatientsInfo(int number = 0)
-        {
-            return View(context.Patients.Include(y => y.Simptoms).Include(j => j.Diagnozs).FirstOrDefault(x => x.Numer == number));
-
-        }
-
-        [HttpDelete]
-        [Route("/PatientsInfoDeliteDiagnoz")]
-        public ActionResult PatientsInfoDeliteDiagnoz(string diagnozId, string patientId)
-        {
-            return Ok();
-        }
-
-        [HttpDelete]
-        [Route("/PatientsInfoDeliteSimptom")]
-        public ActionResult PatientsInfoDeliteSimptom(string simptomId, string patientId)
-        {
-            return Ok();
         }
     }
 }
